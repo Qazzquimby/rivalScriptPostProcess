5caeee2a2aa1aad723fc135964a8037a687407db
Toren Darby <darby@science.ubc.ca>
2020-11-19 13:17:54 -0800

    Add data structures

--- library/data_structures.py
+++ library/data_structures.py
@@ -5,6 +5,7 @@ Map = Define(
     version=0,
     docs="""\
     Creates a mapping from each pair of arguments.
+    See https://docs.yoyogames.com/source/dadiospice/002_reference/data%20structures/ds%20maps/index.html
     my_map = Map(
         "a", 1,
         "b", 2,
@@ -23,6 +24,75 @@ Map = Define(
     return _map;
     """)
 
+List = Define(
+    name="List",
+    version=0,
+    docs="""\
+    Creates an list of the arguments. 
+    See https://docs.yoyogames.com/source/dadiospice/002_reference/data%20structures/ds%20lists/
+    my_list = List(
+        1,
+        2,
+    )
+    my_list[|0]
+    >>> 1
+    """,
+    gml="""\
+    var list = ds_list_create();
+    for (var i = 0; i < argument_count; i++) {
+      ds_list_add(list, argument[i]);
+    }
+    return list;
+    """
+)
+
+Stack = Define(
+    name="Stack",
+    version=0,
+    docs="""\
+    Adds the arguments to the stack from top to bottom (first argument is on top)
+    See https://docs.yoyogames.com/source/dadiospice/002_reference/data%20structures/ds%20stacks/index.html
+    my_stack = Stack(
+        1,
+        2,
+    )
+    ds_stack_pop(my_stack)
+    >>> 1
+    """,
+    gml="""\
+    var stack = ds_stack_create();
+    for (var i = argument_count-1; i >= 0; i--) {
+        ds_stack_push(stack, argument[i]);
+    }
+    return stack;
+    """
+)
+
+Queue = Define(
+    name="Queue",
+    version=0,
+    docs="""\
+    Adds the arguments to the stack in order
+    See https://docs.yoyogames.com/source/dadiospice/002_reference/data%20structures/ds%20queues/index.html
+    my_queue = Queue(
+        1,
+        2,
+    )
+    ds_queue_dequeue(my_queue)
+    >>> 2
+    """,
+    gml="""\
+    var queue = ds_queue_create();
+    for (var i = 0; i < argument_count; i++) {
+      ds_queue_enqueue(queue, argument[i]);
+    }
+    return queue;
+    """
+)
+
 DEPENDENCIES = [
-    Map
+    Map,
+    List,
+    Stack,
+    Queue
 ]
