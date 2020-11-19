dbe5627567bbf1acda1d0a861ac2d0da46af0ad1
Toren Darby <darby@science.ubc.ca>
2020-11-19 13:17:33 -0800

    Fix docstring in comparisons.py

--- library/comparisons.py
+++ library/comparisons.py
@@ -4,7 +4,7 @@ is_in_range = script_process.dependencies.Define(
     name="is_in_range",
     version=0,
     docs="""\
-    Prints each parameter to console, separated by spaces.""",
+    Returns whether the value falls within the min and max, inclusive.""",
     params=['_value', '_min', '_max'],
     gml="""\
     return _value == clamp(_value, _min, _max)
